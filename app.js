c0a5f163adf2d14b92ef3641768810d01b5c5959
Leonardo Petrauskas - Tecnologia da Informação <leonardo.petrauskas@unimedsa.com.br>
2018-04-25 15:45:44 -0300

    Primeiro commit, inicialização do repositório

---
+++ app.js
@@ -0,0 +1,39 @@
+const express = require('express');
+const app = express();
+const fs = require('fs');
+
+const loadData = (err, data) => {
+    if (err) throw err;
+    data = JSON.parse(data);
+    console.log(data);
+    createRoutes(data);
+}
+
+const createRoutes = data => {
+    app.get('/', (req, res) => {
+        res.send(data);
+    });
+
+    app.post('/edit/:id/', (req, res) => {
+        for (var i = 0; i < data.length; i++) {
+            if (data[i].id == req.params.id) {
+                data[i].name = req.query.name;
+            }
+        }
+        fs.writeFile('./heroes.json', JSON.stringify(data), {
+            "encoding": "utf-8"
+        }, () => {
+            res.send("Salvo");
+        });
+
+    });
+
+    app.get('/heroes', (req, res) => {
+        res.send(data)
+    })
+    app.listen(3000, () => {
+        console.log("Online");
+    });
+}
+
+fs.readFile('./heroes.json', loadData);
---
+++ readme.md
@@ -0,0 +1,5 @@
+API-HEROES-ANGULAR
+
+API para pesquisa e alteração de dados com uso do file system, para utilizar no tutorial do Angular.
+
+Autor: Leonardo F Petrauskas
